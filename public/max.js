86453e646e16869e562359b8bfa8c1d620e325fc
Adam Baylin <vip.phamquangthinh@gmail.com>
2019-08-10 23:40:40 +0700

    Lesson 21

---
+++ public/max.js
@@ -0,0 +1,64 @@
+var list;
+class Note extends React.Component {
+	render() {
+		return(
+			<div className="div-note">
+				{this.props.children}
+			</div>
+		);
+	}
+}
+class InputDiv extends React.Component {
+	send(){
+		list.setState({
+			mang : list.state.mang.concat(this.refs.txt.value)
+		});
+	}
+	constructor(props) {
+		super(props);
+		this.send = this.send.bind(this);
+	}
+	render() {
+		return (
+			<div>
+				<input type='text' ref='txt' placeholder=' Enter Your Note !'/> 
+				<button onClick={this.send}>Sent</button>
+			</div>
+			);
+	}
+}
+
+class List extends React.Component {
+	constructor (props) {
+		super(props);
+		list = this;
+		this.state = {
+			mang: ["Hello World !",  "Project JS", "By Adam Baylin"],
+		}
+
+	}
+	render() {
+		return (
+		 <div className='div-list'>
+		 	<div id='div-add'></div>
+		 	<button onClick={addDiv}>Add</button>
+		 	{
+		 		this.state.mang.map(function(note, index){
+		 		return <Note key={index}> {note} </Note>
+		 		})
+		 	}
+		 </div>
+		);
+	}
+}
+
+function addDiv() {
+	ReactDOM.render(<InputDiv />, document.getElementById('div-add'));
+};
+
+ReactDOM.render(
+	<div>
+		<List/>
+	</div>
+	, document.getElementById('root')
+);
